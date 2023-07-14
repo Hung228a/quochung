e2bbb77bb656bc325b44da64255dc5c9f0b5c638
quochung228a@gmail.com <quochung228a@gmail.com>
2023-07-14 23:50:43 +0700

    1

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,12 @@
+const MyConstants = {
+  DB_SERVER: 'cluster0.ctrb521.mongodb.net',
+  DB_USER: 'quochung228a',
+  DB_PASS: '011102',
+  DB_DATABASE: 'shoppingonline',
+  EMAIL_USER: 'quochung228a@gmail.com', // Microsoft mail service
+  EMAIL_PASS: 'onfxjpqhnxjqifhz',
+  JWT_SECRET: '<assssssd>',
+  JWT_EXPIRES: '1d', // in milliseconds onfxjpqhnxjqifhz
+};
+module.exports = MyConstants;
+
